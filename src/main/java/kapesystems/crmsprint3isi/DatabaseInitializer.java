ef8a3c218d27af8072cf7e20a019849cd9cf38d0
RamsayBoy <38114695+RamsayBoy@users.noreply.github.com>
2019-12-03 19:58:42 +0100

    Update DatabaseInitializer.java
    Now contains the ContactRepository and some contacts but it does not work yet

--- src/main/java/kapesystems/crmsprint3isi/DatabaseInitializer.java
+++ src/main/java/kapesystems/crmsprint3isi/DatabaseInitializer.java
@@ -1,14 +1,27 @@
 package kapesystems.crmsprint3isi;
 
-import kapesystems.crmsprint3isi.repositories.ClientRepository;
+import kapesystems.crmsprint3isi.model.Contact;
+import kapesystems.crmsprint3isi.repositories.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 
-public class DatabaseInitializer {
+public class DatabaseInitializer implements CommandLineRunner {
+
+    @Autowired
+    private ContactRepository clientRepo;
+
+    public DatabaseInitializer(ContactRepository clientRepo) {
+        this.clientRepo = clientRepo;
+    }
 
     @Override
     public void run(String... args) throws Exception {
 
+        clientRepo.save(new Contact("Manolo", "El del bombo"));
+        clientRepo.save(new Contact("Pepe", "El del carrillo"));
+        clientRepo.save(new Contact("Barajas", "Tira pa' la fregoneta"));
+        clientRepo.save(new Contact("Juan", "Guerrero despiadado"));
 
+        System.out.println("Database initialized successfully");
     }
 }
