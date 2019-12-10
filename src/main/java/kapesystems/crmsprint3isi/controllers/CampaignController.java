61734519cda1f790f8f1660f8894474009698c12
RamsayBoy <38114695+RamsayBoy@users.noreply.github.com>
2019-12-10 19:39:19 +0100

    Add edit campaign

--- src/main/java/kapesystems/crmsprint3isi/controllers/CampaignController.java
+++ src/main/java/kapesystems/crmsprint3isi/controllers/CampaignController.java
@@ -5,8 +5,10 @@ import kapesystems.crmsprint3isi.repositories.CampaignRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
@@ -38,7 +40,24 @@ public class CampaignController {
         Optional campaign = campaignRepo.findById(id);
 
         if(campaign.isPresent()) {
-            redirectAttributes.addFlashAttribute("campaign", campaign.get());
+            Campaign campaignToEdit = (Campaign) campaign.get();
+            redirectAttributes.addFlashAttribute("campaign", campaignToEdit);
+        }
+
+        return "redirect:/campaigns";
+    }
+
+    @RequestMapping(value = "/post/{id}", method = RequestMethod.POST)
+    public String postCampaign(@PathVariable Long id, @ModelAttribute("campaign") Campaign campaignEdited,
+                               RedirectAttributes redirectAttributes) {
+        Optional<Campaign> campaign = campaignRepo.findById(id);
+
+        if(campaign.isPresent()) {
+            campaignRepo.save(campaignEdited);
+            redirectAttributes.addFlashAttribute("editClientMsg", "La campaña ha sido modificada");
+        }
+        else {
+            redirectAttributes.addFlashAttribute("redirectErrMsg", "El cliente no se ha podido editar");
         }
 
         return "redirect:/campaigns";
