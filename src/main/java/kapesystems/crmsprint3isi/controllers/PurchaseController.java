8ce8fe63369c0e9ccc6239d6036b107c034e7feb
RamsayBoy <38114695+RamsayBoy@users.noreply.github.com>
2019-12-11 17:50:38 +0100

    Add see and edit purchases

--- src/main/java/kapesystems/crmsprint3isi/controllers/PurchaseController.java
+++ src/main/java/kapesystems/crmsprint3isi/controllers/PurchaseController.java
@@ -1,13 +1,20 @@
 package kapesystems.crmsprint3isi.controllers;
 
+import kapesystems.crmsprint3isi.model.Campaign;
+import kapesystems.crmsprint3isi.model.Client;
 import kapesystems.crmsprint3isi.model.Purchase;
 import kapesystems.crmsprint3isi.repositories.PurchaseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("purchases")
@@ -75,5 +82,65 @@ public class PurchaseController {
             model.addAttribute("earnedMoney" + i, earnedMoney[i]);
 
         return "purchases";
+    }
+
+    @RequestMapping("/{id}")
+    public String purchase(Model model, @PathVariable long id) {
+        Optional<Purchase> purchase = purchaseRepo.findById(id);
+
+        if(purchase.isPresent()) {
+            model.addAttribute("purchase", purchase.get());
+        }
+
+        return "purchase";
+    }
+
+    @RequestMapping("/create")
+    public String createPurchase(RedirectAttributes redirectAttributes) {
+        return "redirect:/purchases/create";
+    }
+
+    @RequestMapping("/add")
+    public String addPurchase(@ModelAttribute("purchase")Purchase newPurchase, RedirectAttributes redirectAttributes) {
+        if(newPurchase != null) {
+            purchaseRepo.save(newPurchase);
+            redirectAttributes.addFlashAttribute("redirectMsg", "La compra " + newPurchase.getProductName()
+                    + " ha sido añadida con éxito.");
+        }
+        else {
+            redirectAttributes.addFlashAttribute("redirectErrMsg", "Hubo un error al intentar añadir la compra");
+        }
+
+        return "redirect:/purchases";
+    }
+
+    @RequestMapping("{id}/edit")
+    public String editPurchase(RedirectAttributes redirectAttributes, @PathVariable long id) {
+        redirectAttributes.addFlashAttribute("editMode", true);
+
+        Optional purchase = purchaseRepo.findById(id);
+
+        if(purchase.isPresent()) {
+            Purchase purchaseToEdit = (Purchase) purchase.get();
+            redirectAttributes.addFlashAttribute("purchase", purchaseToEdit);
+        }
+
+        return "redirect:/purchases/" + id;
+    }
+
+    @RequestMapping(value = "/post/{id}", method = RequestMethod.POST)
+    public String postPurchase(Model model, @PathVariable Long id, @ModelAttribute("purchase") Purchase purchaseEdited,
+                             RedirectAttributes redirectAttributes) {
+        Optional<Purchase> purchase = purchaseRepo.findById(id);
+
+        if(purchase.isPresent()) {
+            purchaseRepo.save(purchaseEdited);
+            redirectAttributes.addFlashAttribute("editClientMsg", "La compra ha sido modificada");
+        }
+        else {
+            redirectAttributes.addFlashAttribute("redirectErrMsg", "La compra no se ha podido editar");
+        }
+
+        return "redirect:/purchases";
     }
 }
